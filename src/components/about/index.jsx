dee12909d64a199ed4130b67344284d14ef52de2
SaddockAime <aimegetz@gmail.com>
2024-11-20 21:23:27 +0200

    fix bugs

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ItemLayout from "./ItemLayout";
 import Link from "next/link";
-import Image from 'next/image';
 
 const AboutDetails = () => {
   return (
@@ -44,7 +43,7 @@ const AboutDetails = () => {
         <ItemLayout
           className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
         >
-          <Image
+          <img
             className="w-full h-auto"
             src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=saddockAime&theme=transparent&hide_border=true&title_color=ffb703&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
             alt="SaddockAime"
@@ -53,7 +52,7 @@ const AboutDetails = () => {
         </ItemLayout>
 
         <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
-          <Image
+          <img
             className="w-full h-auto"
             src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=saddockAime&theme=transparent&hide_border=true&title_color=ffb703&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
             alt="SaddockAime"
@@ -62,7 +61,7 @@ const AboutDetails = () => {
         </ItemLayout>
 
         <ItemLayout className={"col-span-full"}>
-          <Image
+          <img
             className="w-full h-auto"
             src={`https://skillicons.dev/icons?i=appwrite,bootstrap,cloudflare,css,docker,figma,firebase,git,github,graphql,html,ipfs,js,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,sass,tailwind,threejs,vercel,vite,vscode,yarn`}
             alt="SaddockAime"
@@ -71,7 +70,7 @@ const AboutDetails = () => {
         </ItemLayout>
 
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
-          <Image
+          <img
             className="w-full h-auto"
             src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=ffb703`}
             alt="SaddockAime"
@@ -85,7 +84,7 @@ const AboutDetails = () => {
             target="_blank"
             className="w-full"
           >
-            <Image
+            <img
               className="w-full h-auto"
               src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=saddockAime&repo=My-Brand-Saddock&theme=transparent&hide_border=true&title_color=ffb703&text_color=FFFFFF&icon_color=ffb703&text_bold=false&description_lines_count=2`}
               alt="SaddockAime"
